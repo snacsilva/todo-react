abd6ba9fc2248fe4ec880aa12f74a3cb69d9671f
Samantha Silva <samanthasilva.n@gmail.com>
2019-08-28 10:25:49 -0300

    Adicionando tasks.

--- src/components/tasks/CreateTasks.js
+++ src/components/tasks/CreateTasks.js
@@ -1,1 +1,42 @@
+import React, { useState } from 'react'
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
 
+import api from '../../services/api'
+
+export default function CreateTask(props) {
+  const [title, setTitle] = useState('')
+  const [show, setShow] = useState('')
+  
+  async function handleSubmit() {
+    await api.post('/tasks', { title: title, done: false })
+    setShow(false)
+    setTitle('')
+  }
+
+  return (
+    <div>
+      <Button onClick={e => setShow(true)} variant="dark" className="float-right create_task_btn">+ Tasks</Button>
+   
+      <Modal show={show || false} onHide={e => setShow(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>New Task</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Control type="email" placeholder="Enter with your task..." value={title || ''} onChange={e => setTitle(e.target.value)} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={e => setShow(false)}>
+            Close
+          </Button>
+          <form onSubmit={handleSubmit}>
+            <Button variant="dark" type="submit">
+              Create
+            </Button>
+          </form>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  )
+}
--- src/components/tasks/Task.js
+++ src/components/tasks/Task.js
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from 'react'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import List from './List'
+import CreateTask from './CreateTasks'
+import Button from 'react-bootstrap/Button'
 
 import api from '../../services/api'
 
@@ -21,10 +23,12 @@ export default function Tasks () {
       <Col xs={{span: 8,offset: 2}} className="tasks_list">
         <p className="title"> To-do </p>
         <List tasks={tasks.filter((task) => task.done !== true)}/>
+        <CreateTask />
       </Col>
       <Col xs={{span: 8,offset: 2}} className="tasks_list">
         <p className="title"> Done </p>
         <List tasks={tasks.filter((task) => task.done === true)}/>
+        <Button variant="dark"className="float-right remove_tasks_btn">Remove all tasks</Button>
       </Col>
     </Row>
   )
